00bef4afc8c4be26f15a8c522067b94af2c48737
advancingKoala <milaiduoduo4567@163.com>
2018-05-28 20:01:20 +0800

    在vue中使用jquery. 引用步骤：1、安装jquery模块，npm i jquery -D；  2、在main.js中引用 import $ from 'jquery'；3、在webpack.back.conf.js中，增加对webpack的引用const webpack = require('webpack'); 和对plugins属性的配置，  plugins: [     new webpack.ProvidePlugin({       $: "jquery",       jQuery: 'jquery'     })   ] 4、重启npm run dev

---
+++ jquery/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input type="text" ref="input1">
+  </div>
+</template>
+
+<script>
+  $(function(){
+      alert(1);
+  })
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    mounted(){
+//      let val = $(this.$refs.input1).val;
+//      console.log(val);
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
